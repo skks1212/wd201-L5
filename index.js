f0df19fb0c91bf9ebfbee584a74098fc9b7b8e29
Shivank Kacker <kacker.shivank@gmail.com>
2022-10-12 21:07:43 +0530

    L3

--- index.js
+++ index.js
@@ -6,31 +6,31 @@ const todoList = () => {
     const markAsComplete = (index) => {
         all[index].completed = true
     }
-  
+
     const overdue = () => {
         // Write the date check condition here and return the array of overdue items accordingly.
         return all.filter(item => item.dueDate < formattedDate(new Date()))
     }
-  
+
     const dueToday = () => {
         // Write the date check condition here and return the array of todo items that are due today accordingly.
         return all.filter(item => item.dueDate === formattedDate(new Date()))
     }
-  
+
     const dueLater = () => {
         // Write the date check condition here and return the array of todo items that are due later accordingly.
         return all.filter(item => item.dueDate > formattedDate(new Date()))
     }
-  
+
     const toDisplayableList = (list) => {
         // Format the To-Do list here, and return the output string as per the format given above.
         const dueTodayItems = dueToday();
-        return list.map(item => `[${(item.completed ? "x" : " ")}] ${item.title} ${!dueTodayItems.includes(item) ? ( " " + item.dueDate ) : ""}`).join("\n");
+        return list.map(item => `[${(item.completed ? "x" : " ")}] ${item.title} ${!dueTodayItems.includes(item) ? (" " + item.dueDate) : ""}`).join("\n");
     }
-  
+
     return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
 }
-  
+
 // ####################################### #
 // DO NOT CHANGE ANYTHING BELOW THIS LINE. #
 // ####################################### #
@@ -38,13 +38,17 @@ const todoList = () => {
 const todos = todoList();
 
 const formattedDate = d => {
-  return d.toISOString().split("T")[0]
+    return d.toISOString().split("T")[0]
 }
-  
-var d = new Date();
-const today = formattedDate(d)
-const yesterday = formattedDate(new Date(d.setDate(d.getDate() - 1)))
-const tomorrow = formattedDate(new Date(d.setDate(d.getDate() + 2)))
+
+var dateToday = new Date()
+const today = formattedDate(dateToday)
+const yesterday = formattedDate(
+    new Date(new Date().setDate(dateToday.getDate() - 1))
+)
+const tomorrow = formattedDate(
+    new Date(new Date().setDate(dateToday.getDate() + 1))
+)
 
 todos.add({ title: 'Submit assignment', dueDate: yesterday, completed: false })
 todos.add({ title: 'Pay rent', dueDate: today, completed: true })
